109bdc6cdb469a8d44c193cf9b48cceaef949c41
Shantashree Dey <shantashree.works@gmail.com>
2022-10-09 00:30:51 +0530

    update befor authentication or encryption

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,8 @@
 import express from "express";
+import { register } from "../controllers/auth.js";
 
 const router = express.Router()
 
-router.get("/", (req, res) =>{
-    res.send("Help from Auth end point")
-});
-
+router.post("/register", register);
 
 export default router;
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,10 +1,24 @@
 import express from "express";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotels.js";
+import Hotel from "../models/Hotel.js";
+import {createError} from "../utils/error.js";
+
 
 const router = express.Router()
 
-router.get("/", (req, res) =>{
-    res.send("Help from Auth end point")
-});
+// CREATE --------------------------------------------------------------------------------------------------
+router.post("/", createHotel)
 
+// UPDATE --------------------------------------------------------------------------------------------------
+router.put("/:id", updateHotel)
+
+// DELETE --------------------------------------------------------------------------------------------------
+router.delete("/:id", deleteHotel)
+
+// READ - GET --------------------------------------------------------------------------------------------------
+router.get("/:id", getHotel)
+
+// READ - GET ALL --------------------------------------------------------------------------------------------------
+router.get("/", getHotels)
 
 export default router;
